5f3b7dc353f4a828f92f1c4ac214f8a4117c97f4
afolwalex <51683355+afolwalex@users.noreply.github.com>
2020-11-12 20:10:04 +0100

    changes

---
+++ public/javascripts/library/handle_upload.js
@@ -0,0 +1,12 @@
+$("#file-1").fileinput({
+  theme: 'fas',
+  uploadUrl: '#', // you must set a valid URL here else you will get an error
+  overwriteInitial: false,
+  maxFileSize: 10000,
+  maxFilesNum: 100,
+  dropZoneEnabled: false,
+  //allowedFileTypes: ['image', 'video', 'flash'],
+  slugCallback: function (filename) {
+      return filename.replace('(', '_').replace(']', '_');
+  }
+});
