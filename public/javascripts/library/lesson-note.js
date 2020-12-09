d564b3919fbce635228606865f0247a52db09fe9
iwaloye adetola <markgbolahan@gmail.com>
2020-12-09 12:24:46 -0800

    final push

--- public/javascripts/library/lesson-note.js
+++ public/javascripts/library/lesson-note.js
@@ -1,24 +1,12 @@
 import {selector , getData} from "./api.js" 
 let contentArea = selector("#content")
+let presentationArea = selector("#presentation")
+let evaluationArea = selector("#evaluation")
 let id = contentArea.getAttribute("data-id") 
 getData(`/staff/lesson-note/${id}/content`)
 .then(res => {
     console.log(res.content.contentDetail) 
     contentArea.innerHTML = res.content.contentDetail
+    presentationArea.innerHTML = res.presentation
+    evaluationArea.innerHTML = res.evaluation
 }).catch(err => console.error(err))
-
-// let presentationArea = selector("#presentation")
-// let id = presentationArea.getAttribute("data-id") 
-// getData(`/staff/lesson-note/${id}/presentation`)
-// .then(res => {
-//     console.log(res.presentation) 
-//     presentationArea.innerHTML = res.presentation
-// }).catch(err => console.error(err))
-
-// let contentArea = selector("#content")
-// let id = contentArea.getAttribute("data-id") 
-// getData(`/staff/lesson-note/${id}/content`)
-// .then(res => {
-//     console.log(res.content.contentDetail) 
-//     contentArea.innerHTML = res.content.contentDetail
-// }).catch(err => console.error(err))
